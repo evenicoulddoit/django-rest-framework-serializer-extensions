73569056c5dc5b5cc7912afbdc0f55a36e3129be
Phil Starkey <philipstarkey@users.noreply.github.com>
2024-03-08 20:38:47 +1100

    Updated PyPI metadata

--- setup.py
+++ setup.py
@@ -126,9 +126,11 @@ setup(
         "Operating System :: OS Independent",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Internet :: WWW/HTTP",
     ],
 )
